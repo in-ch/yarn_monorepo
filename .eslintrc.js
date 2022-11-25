5a3667fe7dab50bcf0a147b55b4f9faf96b1b605
in-ch <sxin2949@naver.com>
2022-11-25 13:53:49 +0900

    chore: 프로젝트 세팅

---
+++ .eslintrc.js
@@ -0,0 +1,89 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ['import', '@typescript-eslint', 'react', 'prettier'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:@next/next/recommended',
+    'plugin:json/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'prettier',
+  ],
+  rules: {
+    'import/extensions': 'off',
+    'import/no-cycle': [0, { ignoreExternal: true }],
+    'jsx-a11y/anchor-is-valid': 'off',
+    'no-console': [
+      2,
+      {
+        allow: ['warn', 'error'],
+      },
+    ],
+    'prefer-const': 'off',
+    'prettier/prettier': 'error',
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-filename-extension': [1, { extensions: ['.ts', '.tsx', '.jsx'] }],
+    'react/prop-types': 'off',
+    'react/require-default-props': 'off',
+    'react/jsx-props-no-spreading': ['error', { custom: 'ignore' }],
+    'react/no-unescaped-entities': 'off',
+    'react/function-component-definition': [
+      2,
+      {
+        namedComponents: 'arrow-function',
+        unnamedComponents: 'arrow-function',
+      },
+    ],
+    // needed because of https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/no-use-before-define.md#how-to-use & https://stackoverflow.com/questions/63818415/react-was-used-before-it-was-defined
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': [
+      'error',
+      { functions: false, classes: false, variables: true },
+    ],
+    // note you must disable the base rule as it can report incorrect errors
+    'no-shadow': 'off',
+    '@typescript-eslint/no-shadow': ['error'],
+  },
+  overrides: [
+    {
+      files: ['src/pages/_app.jsx', 'src/pages/_document.jsx'],
+      rules: {
+        'react/jsx-props-no-spreading': 0,
+      },
+    },
+    {
+      files: ['src/pages/api/**/*'],
+      rules: {
+        'no-console': 0,
+      },
+    },
+  ],
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+      },
+    },
+    react: {
+      version: 'detect',
+    },
+  },
+};
---
+++ src/utils/test.test.ts
@@ -0,0 +1,10 @@
+describe('test check test', () => {
+  it('case 1:', () => {
+    const actual = 'test';
+    const expectedValue = 'test';
+
+    expect(actual).toStrictEqual(expectedValue);
+  });
+});
+
+export {};
